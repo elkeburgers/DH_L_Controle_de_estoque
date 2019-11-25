aaa989fd72b4d7ae071736f2446f9f6e71b58252
Elke Burgers <elke.burgers@hotmail.com>
2019-11-25 16:04:00 -0300

    criacao da tabela de imagens

---
+++ database/migrations/2019_11_25_190047_create_products_images.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsImages extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products_images', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->string('url_img', 300);
+            $table->string('description', 300);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products_images');
+    }
+}
